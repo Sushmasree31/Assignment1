f5434a2f106e7cbaca356614b550092a8efe5a48
Sushmasree31 <80396710+Sushmasree31@users.noreply.github.com>
2021-09-07 08:03:08 +0530

    Add files via upload

---
+++ Celsius.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Day1Practice
+{
+    class Celsius
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount of celsius:");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("kelvin = {0}", celsius + 273);
+            Console.WriteLine("Farenheit = {0}", celsius * 18 / 10 + 32);
+        }
+    }
+}
---
+++ Primeornot.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+
+namespace Day1Practice
+{
+    public class Primeornot
+    {
+        static bool isPrime(int n)
+        {
+            int count = 0;
+            for (int i=1;i<=n;i++)
+            {
+                if(n%i==0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a number to check prime number or not");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<count;i++)
+            {
+                Console.WriteLine("Enter a number");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                if(isPrime(n))
+                {
+                    Console.WriteLine("Is a Prime number");
+                }
+                else if((isPrime(n))== false)
+                {
+                    Console.WriteLine("Not a prime number");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
+
+    
