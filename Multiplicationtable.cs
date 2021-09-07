04c5b435737c7c5eb0ee8762fa40229b0a08459f
Sushmasree31 <80396710+Sushmasree31@users.noreply.github.com>
2021-09-07 15:15:10 +0530

    Add files via upload

---
+++ Multiplicationtable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1Practice
+{
+    class Multiplicationtable
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine(" Enter a number");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("Multiplication Table of " + num); ;
+            while(i <= 10)
+            {
+                Console.WriteLine($"{num} * {i} = {num * i}");
+                i++;
+            }
+        }
+    }
+}
